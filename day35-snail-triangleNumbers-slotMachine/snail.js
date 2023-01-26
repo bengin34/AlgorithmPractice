a19bd614d65c3202e1b6424f0147255379f9bddb
bengin34 <118957608+bengin34@users.noreply.github.com>
2023-01-26 09:12:29 +0300

    Update snail.js

--- day35-snail-triangleNumbers-slotMachine/snail.js
+++ day35-snail-triangleNumbers-slotMachine/snail.js
@@ -22,8 +22,4 @@
      return  Math.ceil( ((column - day) / ( day-night))  + 1)
     }
 
-    console.log(snail(3,2,1));
-    console.log(snail(10,3,1));
-    console.log(snail(10,3,2));
-    console.log(snail(100,20,5));
-    console.log(snail(5,10,3));
+  
