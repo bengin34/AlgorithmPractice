b3aa3987b785f4d0ddb8f8916e80bcbc18f325c0
bengin34 <becaglar3434@gmail.com>
2023-01-27 14:09:30 +0300

    fixed

--- day38-WeeklyCodingChallanges/studentRegistration.js
+++ day38-WeeklyCodingChallanges/studentRegistration.js
@@ -20,43 +20,40 @@
 
 // Problem Statement
 // Sample Input
-let data =['St Nr-First Name,Last Name,Location	Path',
-    'C1234 - John Doe, London, Full-Stack',
-'C2345 - Jane Doe, London, Data-Science',
-'C2346 - Mary Ann, Paris, AWS-Devops',
-'C2347 - Adam Smith, Texas, AWS-Devops',
-'C2444 - Michael Great, Arizona, Full-Stack',
-'C2555 - William Cash, Manchester, Data-Science',
-'C2455 - Patrick Jane, Madrid, Full-Stack']
+let data = [
+  "St Nr-First Name,Last Name,Location	Path",
+  "C1234 - John Doe, London, Full-Stack",
+  "C2345 - Jane Doe, London, Data-Science",
+  "C2346 - Mary Ann, Paris, AWS-Devops",
+  "C2347 - Adam Smith, Texas, AWS-Devops",
+  "C2444 - Michael Great, Arizona, Full-Stack",
+  "C2555 - William Cash, Manchester, Data-Science",
+  "C2455 - Patrick Jane, Madrid, Full-Stack",
+];
 
+function createTable(data) {
+  let table = document.createElement("table");
+  table.style.borderCollapse = "collapse";
+  table.style.border = "1px solid white";
+  let tbody = document.createElement("tbody");
+  let theader = document.createElement("theader");
+  table.appendChild(tbody);
+  tbody.appendChild(theader);
 
+  for (let i = 0; i < data.length; i++) {
+    let tr = document.createElement("tr");
+    tbody.appendChild(tr);
+    tr.style.border = " 1px solid white";
 
+    let cells = data[i].replace("-", ",").split(",");
+    for (let j = 0; j < cells.length; j++) {
+        let td = document.createElement("td");
+        td.style.border = " 1px solid white";
+        td.innerHTML = cells[j];
+        tr.appendChild(td);
+    }
+  }
+  document.body.appendChild(table);
+}
 
-
-        function createTable(data) {
-            let table = document.createElement("table");
-            table.style.borderCollapse = "collapse";
-            table.style.border = "1px solid white";
-            let tbody = document.createElement("tbody");
-            let theader = document.createElement("theader")
-            table.appendChild(tbody);
-            tbody.appendChild(theader)
-        
-            for (let i = 0; i < data.length; i++) {
-                let tr = document.createElement("tr");
-                tbody.appendChild(tr);
-                tr.style.border=" 1px solid white"
-                tr.style.padding="4px"
-                let cells = data[i].replace("-", ",").split(',');
-                for (let j = 0; j < cells.length; j++) {
-                    let td = document.createElement("td");
-                    td.style.border= " 1px solid white"
-                    td.style.margin= "2px"
-                    td.innerHTML = cells[j];
-                    tr.appendChild(td);
-                }
-            }
-            document.body.appendChild(table);
-        }
-        
-        createTable(data);
+createTable(data);
