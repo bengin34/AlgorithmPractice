659a0a029036a07d022437fe07ba6afb972d7259
bengin34 <becaglar3434@gmail.com>
2023-02-03 09:09:16 +0300

    day44

---
+++ day44-Cypher-NearestSquare/nearestSquare.js
@@ -0,0 +1,13 @@
+// Your task is to find the nearest square number, nearest_sq(n), of a positive integer n.
+
+// Goodluck :)
+
+// Check my other katas:
+
+// Alphabetically ordered
+
+// Case-sensitive!
+
+// Not prime numbers
+
+const nearestSq = n => Math.pow(Math.round(Math.sqrt(n)), 2);
