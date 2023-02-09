0f3d8b7d96a047ec012620d3434b72ebc640368c
bengin34 <becaglar3434@gmail.com>
2023-02-09 09:39:04 +0300

    done

---
+++ day49-ApplesDisribution-RemoveInArray/RemoveInArrays.js
@@ -0,0 +1,17 @@
+// Some people really wonder why JavaScript arrays don't have a remove function to remove an element.
+
+// Go ahead and make one.
+
+// var someArray = [1, 2, 3];
+// someArray.remove(1);
+// This should remove the element on index 1 making someArray = [1, 3]
+
+// You also need to handle invalid inputs: If index is not a number or out of bounds, return the array unchanged.
+
+var someArray = [1, 2, 3];
+
+Array.prototype.remove = function (index){
+    if(typeof index == 'number' && index >= 0) 
+    this.splice(index,1)
+    return this
+}
